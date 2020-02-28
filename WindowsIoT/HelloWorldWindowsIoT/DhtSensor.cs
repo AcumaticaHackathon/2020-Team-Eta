d5d381db1ffbf4c183ba093ec2c3eec16e3ebb0e
Brendan Hennelly <bhennelly@jaas.net>
2020-02-28 14:28:18 -0500

    Include windows IoT solution

---
+++ WindowsIoT/HelloWorldWindowsIoT/DhtSensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sensors.Dht;
+//using Sensors.OneWire.Common;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+namespace HelloWorldWindowsIoT
+{
+    public class DhtSensor
+    {
+        
+    }
+}
---
+++ WindowsIoT/HelloWorldWindowsIoT/MainPage.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Sensors.Dht;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HelloWorldWindowsIoT
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Windows.UI.Xaml.DispatcherTimer _timer = new Windows.UI.Xaml.DispatcherTimer();
+
+        private const int SENSOR_PIN_NUMBER = 4;
+        GpioPin _pin = null;
+        private IDht _dht = null;
+        private List<int> _retryCount = new List<int>();
+        private DateTimeOffset _startedAt = DateTimeOffset.MinValue;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += this.Timer_Tick;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            GpioController controller = GpioController.GetDefault();
+
+            if (controller != null)
+            {
+                _pin = GpioController.GetDefault().OpenPin(SENSOR_PIN_NUMBER, GpioSharingMode.Exclusive);
+                _dht = new Dht11(_pin, GpioPinDriveMode.Input);
+                _timer.Start();
+                _startedAt = DateTimeOffset.Now;
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            _timer.Stop();
+
+            if (_pin != null)
+            {
+                _pin.Dispose();
+                _pin = null;
+            }
+
+            _dht = null;
+
+            base.OnNavigatedFrom(e);
+        }
+
+        private string GetHostName()
+        {
+            foreach (Windows.Networking.HostName name in Windows.Networking.Connectivity.NetworkInformation.GetHostNames())
+            {
+                if (Windows.Networking.HostNameType.DomainName == name.Type)
+                {
+                    return name.DisplayName;
+                }
+            }
+            return null;
+        }
+
+        private async void SendTemperature(float temperature, float humidity)
+        {
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+            var headers = httpClient.DefaultRequestHeaders;
+
+            var header = "ie";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            var address = string.Format("https://localhost/HackEta/Pages/HK/DataHandler.aspx?temperature={0}&deviceid={1}&humidity={2}", temperature, GetHostName(), humidity);
+            var requestUri = new Uri(address);
+
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+        }
+
+        private async void Timer_Tick(object sender, object e)
+        {
+            DhtReading reading = new DhtReading();
+            int val = this.TotalAttempts;
+            this.TotalAttempts++;
+
+            reading = await _dht.GetReadingAsync().AsTask();
+
+            _retryCount.Add(reading.RetryCount);
+
+            TotalAttempts++;
+
+            if (reading.IsValid)
+            {
+                this.TotalSuccess++;
+                this.Temperature = Convert.ToSingle(reading.Temperature);
+                this.Humidity = Convert.ToSingle(reading.Humidity);
+                SendTemperature(Temperature, Humidity);
+            }
+
+            LastUpdated = DateTimeOffset.Now;
+
+            this.TotalAttemptsMessage.Text = this.TotalAttempts.ToString();
+            this.TotalSuccessfulMessage.Text = this.TotalSuccess.ToString();
+            this.TemperatureMessage.Text = this.TemperatureDisplay;
+            this.HumidityMessage.Text = this.HumidityDisplay;
+        }
+
+        public float Temperature;
+        public int TotalAttempts;
+        public int TotalSuccess;
+        public float Humidity;
+
+        public string HumidityDisplay => GetHumidityDisplay(Humidity);
+
+        private string GetHumidityDisplay(float humidity) => string.Format("{0:0.0}% RH", humidity);
+
+        public string TemperatureDisplay => GetTemperatureDisplay(Temperature);
+        private string GetTemperatureDisplay(float temperature) => string.Format("{0:0.0} °C", temperature);
+
+        private DateTimeOffset _lastUpdated = DateTimeOffset.MinValue;
+        public DateTimeOffset LastUpdated
+        {
+            get
+            {
+                return _lastUpdated;
+            }
+            set
+            {
+                _lastUpdated = value;
+            }
+        }
+
+        public string LastUpdatedDisplay
+        {
+            get
+            {
+                string returnValue = string.Empty;
+
+                TimeSpan elapsed = DateTimeOffset.Now.Subtract(this.LastUpdated);
+
+                if (this.LastUpdated == DateTimeOffset.MinValue)
+                {
+                    returnValue = "never";
+                }
+                else if (elapsed.TotalSeconds < 60d)
+                {
+                    int seconds = (int)elapsed.TotalSeconds;
+
+                    if (seconds < 2)
+                    {
+                        returnValue = "just now";
+                    }
+                    else
+                    {
+                        returnValue = string.Format("{0:0} {1} ago", seconds, seconds == 1 ? "second" : "seconds");
+                    }
+                }
+                else if (elapsed.TotalMinutes < 60d)
+                {
+                    int minutes = (int)elapsed.TotalMinutes == 0 ? 1 : (int)elapsed.TotalMinutes;
+                    returnValue = string.Format("{0:0} {1} ago", minutes, minutes == 1 ? "minute" : "minutes");
+                }
+                else if (elapsed.TotalHours < 24d)
+                {
+                    int hours = (int)elapsed.TotalHours == 0 ? 1 : (int)elapsed.TotalHours;
+                    returnValue = string.Format("{0:0} {1} ago", hours, hours == 1 ? "hour" : "hours");
+                }
+                else
+                {
+                    returnValue = "a long time ago";
+                }
+
+                return returnValue;
+            }
+        }
+
+        public int AverageRetries
+        {
+            get
+            {
+                int returnValue = 0;
+
+                if (_retryCount.Count() > 0)
+                {
+                    returnValue = (int)_retryCount.Average();
+                }
+
+                return returnValue;
+            }
+        }
+
+        public string AverageRetriesDisplay => string.Format("{0:0}", this.AverageRetries);
+
+        public string SuccessRate
+        {
+            get
+            {
+                string returnValue = string.Empty;
+
+                double totalSeconds = DateTimeOffset.Now.Subtract(_startedAt).TotalSeconds;
+                double rate = this.TotalSuccess / totalSeconds;
+
+                if (rate < 1)
+                {
+                    returnValue = string.Format("{0:0.00} seconds/reading", 1d / rate);
+                }
+                else
+                {
+                    returnValue = string.Format("{0:0.00} readings/sec", rate);
+                }
+
+                return returnValue;
+            }
+        }
+    }
+}
